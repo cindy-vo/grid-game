ff4422155ff31bd959f2507019e7c2262b1e4422
Cindy Vo <cindyhvo824@gmail.com>
2023-02-21 23:27:56 -0500

    colored grid and configured movement; still need to fix previous state issue and game over condition

--- grid-game-frontend/src/GridGame.tsx
+++ grid-game-frontend/src/GridGame.tsx
@@ -4,119 +4,31 @@ import Grid from "@mui/material/Grid";
 import Board from "./components/Board";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
+import { KEY_CODES_MAPPER, DIRECTION_TICKS } from "./utilities/constants";
 import {
-  KEY_CODES_MAPPER,
-  DIRECTIONS,
-  DIRECTION_TICKS,
-} from "./utilities/constants";
-import {
-  getRandomCoordinate,
+  getStartAndFinish,
   getRandomNumberFromRange,
   isBorder,
 } from "./utilities/helpers";
 
-const Cell = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+const Cell = styled(Paper)(({ theme, color }) => ({
+  backgroundColor: color,
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
 }));
 
-const getInitialState = () => {
-  const initialState = {
-    playground: {
-      direction: DIRECTIONS.RIGHT,
-      isGameOver: false,
-    },
-    player: {
-      coordinate: getRandomCoordinate(true),
-    },
-    health: 200,
-    moves: 450,
-    pointB: getRandomCoordinate(false),
-    grid: [] as Array<Array<number>>,
-    prevState: 0,
-  };
-  initialState.grid = getGrid2(initialState.player);
-  return initialState;
-};
-
-const initialState = () => getInitialState;
-
-const CELL_TYPES = ["blank", "speeder", "lava", "mud"];
-
-const CELL_TYPES_MAPPING = {
-  blank: { health: 0, moves: -1 },
-  speeder: { health: -5, moves: 0 },
-  lava: { health: -50, moves: -10 },
-  mud: { health: -10, moves: -5 },
-};
-
-const reducer = (state: any, action: any) => {
-  switch (action.type) {
-    case "PLAYER_MOVE":
-      const move = DIRECTION_TICKS[
-        state.playground.direction as keyof typeof DIRECTION_TICKS
-      ](state.player.x, state.player.y);
-
-      if (isBorder(move)) {
-        return state;
-      }
-
-      state.grid[state.player.x][state.player.y] = state.prevState;
-      const savedState = state.grid[state.move.x][state.move.y];
-      state.grid[state.move.x][state.move.y] = 4;
-
-      return {
-        ...state,
-        player: {
-          coordinate: move,
-        },
-        health:
-          state.health -
-          CELL_TYPES_MAPPING[
-            CELL_TYPES[
-              state.grid[move.x][move.y]
-            ] as keyof typeof CELL_TYPES_MAPPING
-          ].health,
-        moves:
-          state.moves -
-          CELL_TYPES_MAPPING[
-            CELL_TYPES[
-              state.grid[move.x][move.y]
-            ] as keyof typeof CELL_TYPES_MAPPING
-          ].moves,
-        prevState: savedState,
-      };
-    case "GAME_OVER":
-      return {
-        ...state,
-        playground: {
-          ...state.playground,
-          isGameOver: true,
-        },
-      };
-    default:
-      throw new Error();
-  }
-};
-
-const getGrid = (player: any) => {
+const getGrid = (board: Array<Array<number>>) => {
   const grid = [] as Array<Array<ReactElement>>;
   for (let i = 0; i < 100; i++) {
     grid[i] = [];
     for (let j = 0; j < 100; j++) {
-      const random_number = getRandomNumberFromRange(0, 3);
+      const color =
+        CELL_TYPES_MAPPING[
+          CELL_TYPES[board[i][j]] as keyof typeof CELL_TYPES_MAPPING
+        ].backgroundColor;
       grid[i].push(
         <Grid item xs={1}>
-          <Cell
-            className={
-              i !== player.coordinates.x && j !== player.coordinates.y
-                ? CELL_TYPES[random_number]
-                : ""
-            }
-          ></Cell>
+          <Cell color={color}></Cell>
         </Grid>
       );
     }
@@ -124,13 +36,18 @@ const getGrid = (player: any) => {
   return grid;
 };
 
-const getGrid2 = (player: any) => {
+const getGrid2 = (
+  player: { coordinate: { x: number; y: number } },
+  finish: { coordinate: { x: number; y: number } }
+) => {
   const grid = [] as Array<Array<number>>;
   for (let i = 0; i < 100; i++) {
     grid[i] = [];
     for (let j = 0; j < 100; j++) {
-      if (i === player.coordinates.x && j === player.coordinates.y) {
+      if (i === player.coordinate.y && j === player.coordinate.x) {
         grid[i].push(4);
+      } else if (i === finish.coordinate.y && j === finish.coordinate.x) {
+        grid[i].push(5);
       } else {
         const random_number = getRandomNumberFromRange(0, 3);
         grid[i].push(random_number);
@@ -138,6 +55,84 @@ const getGrid2 = (player: any) => {
     }
   }
   return grid;
+};
+
+const getInitialState = () => {
+  const initialState = {
+    isGameOver: false,
+
+    player: {
+      coordinate: getStartAndFinish(),
+    },
+    health: 200,
+    moves: 450,
+    pointB: { coordinate: getStartAndFinish() },
+    grid: [] as Array<Array<number>>,
+    prevState: 0,
+  };
+
+  initialState.grid = getGrid2(initialState.player, initialState.pointB);
+  return initialState;
+};
+
+const initialState = getInitialState();
+
+const CELL_TYPES = ["blank", "speeder", "lava", "mud", "player", "finish"];
+
+const CELL_TYPES_MAPPING = {
+  blank: { health: 0, moves: 1, backgroundColor: "#fff" },
+  speeder: { health: 5, moves: 0, backgroundColor: "#42a5f5" },
+  lava: { health: 50, moves: 10, backgroundColor: "#ef5350" },
+  mud: { health: 10, moves: 5, backgroundColor: "#ff9800" },
+  player: { health: 0, moves: 0, backgroundColor: "#ba68c8" },
+  finish: { health: 0, moves: 0, backgroundColor: "black" },
+};
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "PLAYER_MOVE":
+      const move = DIRECTION_TICKS[
+        action.direction as keyof typeof DIRECTION_TICKS
+      ](state.player.coordinate.x, state.player.coordinate.y);
+      console.log(isBorder(move));
+
+      if (isBorder(move)) {
+        return state;
+      }
+      state.grid[state.player.coordinate.y][state.player.coordinate.x] =
+        state.prevState;
+      const savedState = state.grid[move.y][move.x];
+      console.log(savedState);
+      state.health =
+        state.health -
+        CELL_TYPES_MAPPING[
+          CELL_TYPES[savedState] as keyof typeof CELL_TYPES_MAPPING
+        ].health;
+      state.moves =
+        state.moves -
+        CELL_TYPES_MAPPING[
+          CELL_TYPES[savedState] as keyof typeof CELL_TYPES_MAPPING
+        ].moves;
+
+      state.prevState = savedState;
+      state.grid[move.y][move.x] = 4;
+
+      console.log(savedState);
+
+      return {
+        ...state,
+        player: {
+          coordinate: move,
+        },
+      };
+    case "GAME_OVER":
+      return {
+        ...state,
+        isGameOver: true,
+      };
+    default:
+      throw new Error();
+  }
 };
 
 const GridGame: React.FunctionComponent = () => {
@@ -158,14 +153,13 @@ const GridGame: React.FunctionComponent = () => {
     return () => window.removeEventListener("keyup", controlMove, false);
   }, []);
 
+  React.useEffect(() => {
+    console.log(state);
+  }, [state]);
+
   return (
     <div className="App">
-      <Board
-        grids={() => {
-          getGrid(state.player);
-        }}
-        state={state}
-      ></Board>
+      <Board grids={getGrid(state.grid)} state={state}></Board>
     </div>
   );
 };
--- grid-game-frontend/src/utilities/helpers.ts
+++ grid-game-frontend/src/utilities/helpers.ts
@@ -1,17 +1,20 @@
 
 import { GRID_LENGTH } from "./constants";
 
+
 const getRandomNumberFromRange = (min:number, max: number) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
-const getRandomCoordinate = (isA: boolean) => ({
-  x: isA ? 1: GRID_LENGTH - 1,
-  y: getRandomNumberFromRange(1, GRID_LENGTH - 1),
-});
+const SPAWN_AREAS = [{x: 0, y: getRandomNumberFromRange(0, GRID_LENGTH - 1)}, {x: getRandomNumberFromRange(0, GRID_LENGTH - 1), y: 0}, {x: getRandomNumberFromRange(0, GRID_LENGTH - 1), y: GRID_LENGTH - 1}, {x: GRID_LENGTH - 1, y: getRandomNumberFromRange(0, GRID_LENGTH - 1)}]
+
+const getStartAndFinish = () => {
+  const idx = getRandomNumberFromRange(0, SPAWN_AREAS.length - 1);
+  const save = SPAWN_AREAS[idx];
+  SPAWN_AREAS.splice(idx, 1);
+  return save;
+}
 
 const isBorder = (coordinate: {x: number, y: number}) =>
-coordinate.x === 0 || coordinate.y === 0 || coordinate.x === GRID_LENGTH || coordinate.y === GRID_LENGTH;
+coordinate.x < 0 || coordinate.y < 0 || coordinate.x === GRID_LENGTH || coordinate.y === GRID_LENGTH;
 
-const isPosition = (x: number, y: number, diffX: number, diffY: number) => x === diffX && y === diffY;
-
-export {getRandomNumberFromRange, getRandomCoordinate, isBorder}
+export {getRandomNumberFromRange, isBorder, getStartAndFinish}
